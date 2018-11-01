a9c295c197886d54bac9b800952cf5bee226488b
haniokasai <htek@haniokasai.com>
2018-11-02 01:14:51 +0900

    pomの誤り

--- src/main/java/com/haniokasai/lsofParser/Main.java
+++ src/main/java/com/haniokasai/lsofParser/Main.java
@@ -1,8 +1,12 @@
 package com.haniokasai.lsofParser;
 
+import static com.haniokasai.lsofParser.Parse.islsofEnable;
+
 public class Main {
     public static boolean debug = true;
     public static void main(String args[]) {
-
+        if(debug){
+            System.out.println(islsofEnable());
+        }
     }
 }
